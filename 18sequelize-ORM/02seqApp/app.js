35f79f13bef1e9a61c72251e32f4f3eaab02337d
Can Baran GÜNDÜZ <canbaran011@gmail.com>
2020-03-27 15:29:27 +0300

    sequelize ..

---
+++ 18sequelize-ORM/02seqApp/app.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// const connection = require('./utility/database');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+
+
+const errorController= require('./controllers/errors');
+const sequelize = require('./utility/database');
+
+const Category = require('./models/category');
+const Product = require('./models/product');
+const User = require('./models/user');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
+app.use((req,res,next) =>{
+    User.findByPk(1)
+    .then(user => {
+        req.user =user;
+        next();
+    }) 
+    .catch(err =>{
+        console.log(err);
+    })
+
+});
+
+// sequelize
+// .authenticate()
+// .then(()=> {
+//   console.log('Connection has been established successfully.');
+// })
+// .catch((err) => {
+//   console.log('Unable to connect to the database:', err);
+// });
+
+
+/*
+connection.execute('SELECT name,price FROM products')
+.then((result) => {
+    console.log(result[0]);
+}).catch((err) =>{
+    console.log(err);
+})
+*/
+
+app.use(errorController.get404Page);
+
+Product.belongsTo(Category,{
+    foreignKey:{
+        allowNull:false
+    }
+
+});
+Category.hasMany(Product);
+
+Product.belongsTo(User);
+User.hasMany(Product);
+
+
+
+sequelize 
+// .sync({force:true})
+.sync()
+.then(() =>{
+
+    User.findByPk(1)
+    .then(user =>{
+        if(!user){
+          return User.create({name:'canbaran',
+            email: 'canbaran@gmail.com'});
+        }
+        return user; 
+    })
+        .then(user => {
+            Category.count()
+                .then(count => {
+
+                    if (count === 0) {
+                        Category.bulkCreate([
+                            { name: 'Telefon', description: 'tel categorisi' },
+                            { name: 'Bilgisayar', description: 'bil categorisi' },
+                            { name: 'Elektronik', description: 'elk categorisi' }
+
+                        ]);
+                    }
+                });
+        })
+})
+.catch(err => {
+    console.log(err);
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
---
+++ 18sequelize-ORM/02seqApp/utility/database.js
@@ -0,0 +1,44 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('node-app','root','mysql1234',{
+    dialect : 'mysql',
+    host : 'localhost'
+
+});
+
+module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
