f289ac3f40c5f9213c3caafdc0c34c7b82be1d63
Can Baran GÜNDÜZ <canbaran011@gmail.com>
2020-04-02 09:34:27 +0300

    noSql first

---
+++ 19noSQL-Mongo/noSql/app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+
+
+const errorController= require('./controllers/errors');
+const mongoConnect = require('./utility/database').mongoConnect;
+
+
+const User = require('./models/user');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next) => {
+    User.findByUserName('canbaran')
+    .then(user => {
+        req.user = new User(user.name,user.email,user._id);
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+//yes or
+
+
+// routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
+
+app.use(errorController.get404Page);
+
+mongoConnect( () => {
+
+    User.findByUserName('canbaran')
+    .then(user =>{
+        if(!user){
+            user = new User('canbaran','canbaran@mail.com');
+            return user.save();
+        }
+        return user;
+    })
+    .then(user=>{
+        console.log(user);
+        app.listen(3000);
+
+    })
+    .catch(err => {
+        console.log(err );
+    })
+
+    // console.log(client);
+});
+
+
