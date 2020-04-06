e7438e54f79e02eb8daad4bf52ae50479dbdf826
Can Baran GÜNDÜZ <canbaran011@gmail.com>
2020-04-06 19:45:51 +0300

    node auth

---
+++ 21auth/auth/middleware/csrf.js
@@ -0,0 +1,24 @@
+
+module.exports = ( req , res , next) => {
+
+    res.locals.csrfToken= req.csrfToken();
+    res.locals.isAuthenticated = req.session.isAuthenticated;
+    next();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
